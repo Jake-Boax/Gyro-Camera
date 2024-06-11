721b6760c9481d0d853196082c4df1b74ac0e5e4
Jake-Boax <125539955+Jake-Boax@users.noreply.github.com>
2024-06-11 13:22:09 +0800

    Update motors.py

--- rpi/motors.py
+++ rpi/motors.py
@@ -15,38 +15,32 @@ def NumCheck(num):
         result = math.ceil(nums[num] / 100) * 100
         return result
 
-
-m1 = 23
+def motorSetUp():
+    m1 = 23
 m2 = 25
 pwm = pigpio.pi() 
 pwm2 = pigpio.pi() 
 pwm.set_mode(m1, pigpio.OUTPUT)
 pwm2.set_mode(m2, pigpio.OUTPUT)
-
 pwm.set_PWM_frequency(m1, 50)
 pwm.set_PWM_range(m1, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
 pwm2.set_PWM_frequency(m2, 50)
 pwm2.set_PWM_range(m2, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
-
 sleep(5)
-
 oldx = 0
 oldy = 0
 oldz = 0
 
-while True:
-    nums = Read()
-
+def MotorControl():
+        nums = Read()
     x = NumCheck(0)
     y = NumCheck(1)
     z = NumCheck(2)
-
     print(x, y, z)
     if x == oldx:
         continue
     else:
         pwm.set_servo_pulsewidth(m1, x)
-
     if y == oldy:
         continue
     else:
@@ -55,3 +49,8 @@ while True:
     oldx = x
     oldy = y
     oldz = z
+
+MotorSetUp();
+
+while True:
+    MotorControl();
